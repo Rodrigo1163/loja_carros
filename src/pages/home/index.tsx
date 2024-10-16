8d3884695af514c4fd72f288f3396d7805db2224
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2024-10-15 20:02:04 -0400

    funcionalidade de deleta carros e de visualizar somentes o carro do usuário cadastrado

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,68 @@
+import { useEffect, useState } from "react";
 import { Container } from "../../components/container";
+import { Link } from "react-router-dom";
+
+import {
+    collection,
+    query,
+    getDocs,
+    orderBy
+} from 'firebase/firestore';
+import { db } from "../../services/firebaseConnection";
+
+interface CarsProps {
+    id: string
+    name: string
+    year: string
+    uid: string
+    price: string | string
+    city: string
+    km: string
+    images: CarImageProps[]
+}
+
+interface CarImageProps {
+    name: string
+    uid: string
+    url: string
+}
 
 export function Home() {
+    const [cars, setCars] = useState<CarsProps[]>([]);
+    const [loadImages, setLoadImages] = useState<string[]>([])
+
+    useEffect(() => {
+        function loadCars() {
+            const carsRef = collection(db, "cars");
+            const queryRef = query(carsRef, orderBy("created", "desc"))
+
+            getDocs(queryRef)
+                .then((snapshot) => {
+                    let listcars = [] as CarsProps[];
+
+                    snapshot.forEach(doc => {
+                        listcars.push({
+                            id: doc.id,
+                            name: doc.data().name,
+                            year: doc.data().year,
+                            km: doc.data().km,
+                            city: doc.data().city,
+                            price: doc.data().price,
+                            images: doc.data().images,
+                            uid: doc.data().uid
+                        })
+                    })
+                    setCars(listcars)
+                })
+        }
+
+        loadCars()
+    }, [])
+
+    function handleImageLoad(id: string) {
+        setLoadImages((prevImageLoaded) => [...prevImageLoaded, id])
+    }
+
     return (
         <Container>
             <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -21,24 +83,36 @@ export function Home() {
 
             <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
 
-                <section className="w-full bg-white rounded-lg">
-                    <img
-                        className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-                        src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202410/20241003/volvo-c40-p8-recharge-twin-electric-ultimate-awd-wmimagem19552690027.webp?s=fill&w=1920&h=1440&q=75"
-                        alt="carro"
-                    />
-                    <p className="font-bold mt-1 mb-2 px-2">BMW 320i</p>
-                    <div className="flex flex-col px-2">
-                        <span className="text-zinc-700 mb-6">Ano 2026/2026 | 23.000 km</span>
-                        <strong className="text-black font-medium text-xl">R$ 190.000</strong>
-                    </div>
+                {cars.map(car => (
 
-                    <div className="w-full h-px bg-slate-200 my-2"></div>
+                    <Link to={`/car/${car.id}`} key={car.id}>
+                        <section className="w-full bg-white rounded-lg">
+                            <div
+                                className="w-full h-72 rounded-lg bg-slate-200"
+                                style={{ display: loadImages.includes(car.id) ? "none" : "block" }}
+                            ></div>
+                            <img
+                                className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                                src={car.images[0].url}
+                                alt="carro"
+                                onLoad={() => handleImageLoad(car.id)}
+                                style={{ display: loadImages.includes(car.id) ? "block" : "none" }}
+                            />
+                            <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
+                            <div className="flex flex-col px-2">
+                                <span className="text-zinc-700 mb-6">Ano {car.year} | {car.km} km</span>
+                                <strong className="text-black font-medium text-xl">R$ {car.price}</strong>
+                            </div>
 
-                    <div className="px-2 pb-2">
-                        <span className="text-zinc-700">Campo Grande - MS</span>
-                    </div>
-                </section>
+                            <div className="w-full h-px bg-slate-200 my-2"></div>
+
+                            <div className="px-2 pb-2">
+                                <span className="text-zinc-700">{car.city}</span>
+                            </div>
+                        </section>
+                    </Link>
+
+                ))}
 
             </main>
         </Container>
